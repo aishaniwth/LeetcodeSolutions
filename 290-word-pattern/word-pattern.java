27d8b93ddb552aa436253ab7b23d174ac19b45b6
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-05 05:13:26 +0530

    Time: 2 ms (15.40%) | Memory: 42.8 MB (24.99%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -6,30 +6,38 @@ if (pattern.length() != mod.length ) {
 }
 
 
-Map <Character , Integer> mpat = new HashMap <>();
-Map <String , Integer> ms = new HashMap <>();
+// Map <Character , Integer> mpat = new HashMap <>();
+// Map <String , Integer> ms = new HashMap <>();
 Map<Character, String> id = new HashMap <>();
 
 char [] ch = pattern.toCharArray();
 for(int i =0; i<mod.length;i++){
 
-mpat.put(ch[i], mpat.getOrDefault(ch[i],0)+1);
-ms.put(mod[i], ms.getOrDefault(mod[i],0)+1);
+// mpat.put(ch[i], mpat.getOrDefault(ch[i],0)+1);
+// ms.put(mod[i], ms.getOrDefault(mod[i],0)+1);
 
-if ( mpat.size() != ms.size() ) {return false;}
+// if ( mpat.size() != ms.size() ) {return false;}
 
 
-else {
+// else {
 if(id.containsKey(ch[i])) {
     if(!id.get(ch[i]) .equals (mod[i])){
         return false;
     }
 }
-id.put(ch[i],mod[i]);
 
-
-
+//else if (!id.containsKey(ch[i]))
+else{
+    if(id.containsValue(mod[i]) ){
+        return false;
+    }
+    id.put(ch[i], mod[i]);
 }
+
+
+
+
+
 
 
 }
