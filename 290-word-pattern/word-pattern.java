a9173b1b5e389eae9ff7174c54187c49cf1d8e9c
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-05 05:01:34 +0530

    Time: 2 ms (15.40%) | Memory: 42.9 MB (14.44%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+String [] mod = s.split("\\s");
+if (pattern.length() != mod.length ) {
+    return false;
+}
+
+
+Map <Character , Integer> mpat = new HashMap <>();
+Map <String , Integer> ms = new HashMap <>();
+Map<Character, String> id = new HashMap <>();
+
+char [] ch = pattern.toCharArray();
+for(int i =0; i<mod.length;i++){
+
+mpat.put(ch[i], mpat.getOrDefault(ch[i],0)+1);
+ms.put(mod[i], ms.getOrDefault(mod[i],0)+1);
+
+if ( mpat.size() != ms.size() ) {return false;}
+
+
+else {
+if(id.containsKey(ch[i])) {
+    if(!id.get(ch[i]) .equals (mod[i])){
+        return false;
+    }
+}
+id.put(ch[i],mod[i]);
+
+
+
+}
+
+
+}
+
+return true ;
+
+
+    }
+}
