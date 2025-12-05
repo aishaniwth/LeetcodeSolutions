b6e12e064d445e0e57002d26e5a2ebedde88c5df
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-05 21:28:21 +0530

    Time: 18 ms (10.73%) | Memory: 47.6 MB (5.87%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -4,23 +4,23 @@ class Solution {
             return false;
         }
 Map<Character , Integer> map = new HashMap<> ();
-        for (int i =0; i<ransomNote.length();i++){
-            map.put(ransomNote.charAt(i), map.getOrDefault(ransomNote.charAt(i),0)+1);
+        for (int i =0; i<magazine.length();i++){
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i),0)+1);
 
         }
- for (int i =0; i<magazine.length();i++){
-           if(map.containsKey(magazine.charAt(i))){
-map.put(magazine.charAt(i) , map.get(magazine.charAt(i)) -1);
-if(map.get(magazine.charAt(i))==0){
-    map.remove( magazine.charAt(i)  ) ;
-}
-
-if(map.isEmpty()) {return true;}
+ for (int i =0; i<ransomNote.length();i++){
+           if(!map.containsKey(ransomNote.charAt(i))  ||  map.get(ransomNote.charAt(i)) <=0 ){
+return false;
 
            }
+
+map.put(  ransomNote.charAt(i) ,      map.get(ransomNote.charAt(i))-1        );
+
+
+
         }
 
-return false;
+return true;
 
     }
 }
