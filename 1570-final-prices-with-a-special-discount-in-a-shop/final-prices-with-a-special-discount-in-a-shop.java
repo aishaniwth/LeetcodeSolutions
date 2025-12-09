14583be27ad6f05e25af8182dca21cd574cb3eec
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-09 05:51:01 +0530

    Time: 4 ms (47.18%) | Memory: 45 MB (75.79%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         Stack<Integer> st = new Stack <> ();
-        int [] res = new int [prices.length];
+        //int [] res = new int [prices.length];
        for(int i = 0 ;i<prices.length;i++){
         while(!st.isEmpty() && prices[st.peek()]>=prices[i]){
-            res[st.peek()] = prices[st.peek()]-prices[i];
+            prices[st.peek()] = prices[st.peek()]-prices[i];
             st.pop();
 
         }
         st.push(i);
-        res[i] = prices[i];
+        //res[i] = prices[i];
        } 
-        return res ;
+        return prices ;
     }
 }
