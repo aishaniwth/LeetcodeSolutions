07447f541da8e9b37c9da3001bc6ede465afb332
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-14 19:23:22 +0530

    Time: 39 ms (26.41%) | Memory: 138 MB (15.09%) - LeetSync

---
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumPossibleSize(int[] nums) {
+        Stack <Integer> st = new Stack <> ();
+        for(int i = 0 ;i < nums.length ; i++){
+            if(!st.isEmpty() && st.peek() > nums[i]){
+                continue;
+            }
+            st.push(nums[i]);
+        } return st.size();
+    }
+}
