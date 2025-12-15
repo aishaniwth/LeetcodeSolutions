938f707aabad8291e30cd685dbad01b4114f5eee
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2025-12-15 20:54:56 +0530

    Time: 145 ms (27.44%) | Memory: 134.8 MB (8.84%) - LeetSync

---
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.java
@@ -0,0 +1,45 @@
+class Solution {
+    public long bowlSubarrays(int[] nums) {
+        Stack <Integer> st = new Stack <> ();
+
+int [] nge =new int [nums.length];
+
+int [] pge = new int [nums.length]; 
+int counter=0;
+
+for (int i = 0 ; i < nums.length;i++){
+nge[i]=-1;
+pge[i]=-1;
+}
+
+
+
+
+for (int i = 0 ; i <nums.length; i++){
+while(!st.isEmpty() && nums[st.peek()] < nums[i] ){
+nge[st.peek()]=i;
+st.pop();
+
+} st.push(i);
+
+}
+st.clear() ; 
+
+for (int i = nums.length-1 ; i >= 0; i--){
+while(!st.isEmpty() && nums[st.peek()] < nums[i] ){
+pge[st.peek()]=i;
+st.pop();
+
+} st.push(i);
+
+}
+
+
+for(int i = 0 ; i < nums.length ; i++ ) {
+if(nge[i]==-1 || pge[i] ==-1) continue; 
+if(nge[i]+pge[i] >=2 ) {counter++;}
+} 
+
+return counter;
+    }
+}
