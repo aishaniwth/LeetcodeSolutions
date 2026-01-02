60d05156d28df82d3f8650c7c460bf17a7b5febb
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2026-01-02 15:28:50 +0530

    Time: 1 ms (99.84%) | Memory: 46.5 MB (55.54%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,71 +10,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-
-int sum = -1;
-int carry = 0 ;
-ListNode curr1 = l1;
-ListNode curr2=l2;
-ListNode ans=new ListNode();
-ListNode ptr = ans ;
-while(curr1!=null && curr2!=null){
-    sum = (curr1.val+curr2.val) + carry;
-    carry=0;
-    if(sum>=10){ 
-    carry=1;
-    }
-    if(curr1.next==null && curr2.next==null && sum>=10){
-ans.next=new ListNode(sum%10);
-ans.next.next=new ListNode(1);
-curr1=curr1.next ;
-    curr2=curr2.next;
-break;
-    }
-ans.next=new ListNode(sum%10);
-ans=ans.next;
-    curr1=curr1.next ;
-    curr2=curr2.next;
-
-
+        ListNode ptr1 = l1;
+        ListNode ptr2  = l2;
+        ListNode ans = new ListNode();
+        ListNode dummy = ans;
+        int sum = 0 ; int carry =0 ;
+        while(ptr1!=null || ptr2!=null || carry==1){
+if(ptr1!=null){
+    sum=sum+ptr1.val;
+    ptr1=ptr1.next;
 }
 
-    while(curr1!=null){
-        sum=curr1.val+carry;
-        carry=0;
-        if(sum>=10){carry=1;}
-        if(curr1.next==null && sum>=10){
-            ans.next=new ListNode (sum%10);
-            ans.next.next= new ListNode (1);
-            break;
+if(ptr2!=null){
+    sum=sum+ptr2.val;
+    ptr2=ptr2.next;
+}
+sum=sum+carry;
 
-        }
-        ans.next= new ListNode(sum%10);
-        ans=ans.next;
-        curr1=curr1.next;
+carry=sum>=10?1:0;
 
-    }
+ans.next = new ListNode(sum%10);
+ans=ans.next;
+
+sum=0;
 
 
- while(curr2!=null){
-        sum=curr2.val+carry;
-        carry=0;
-        if(sum>=10){carry=1;}
-        if(curr2.next==null && sum>=10){
-            ans.next=new ListNode (sum%10);
-            ans.next.next= new ListNode (1);
-            break;
-
-        }
-        ans.next= new ListNode(sum%10);
-        ans=ans.next;
-        curr2=curr2.next;
-
-    }
-
-
-return ptr.next;
-
-
+        } return dummy.next ;
+        
     }
 }
