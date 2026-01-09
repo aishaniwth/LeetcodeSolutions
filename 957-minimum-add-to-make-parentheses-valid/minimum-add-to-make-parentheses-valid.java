c28c4fc788152e26f8fe27e3aa64912bde737d41
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2026-01-09 21:00:11 +0530

    Time: 3 ms (4.14%) | Memory: 43 MB (21.02%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,15 +1,16 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack <> ();
-        for(int i =0 ;i<s.length();i++){
-            if(!st.isEmpty() && s.charAt(i)==')' && st.peek()=='('){
-                st.pop();
-            }
-            else if (s.charAt(i)=='(' || s.charAt(i)==')'){
-                st.push(s.charAt(i));
-                
-            } else {continue;}
-        }
-        return st.size();
+   Stack <Character> st = new Stack<>();
+for(int i = 0 ; i <s.length();i++){
+  if(!st.isEmpty() && st.peek()=='(' && s.charAt(i)==')' ){
+    st.pop();
+  }
+
+
+  else {st.push(s.charAt(i));}
+}
+
+return st.size();
+
     }
 }
