ebba4d0780621550034dc2508be632caa33883d6
Aishani Maheshwari <135871998+aishaniwth@users.noreply.github.com>
2026-01-15 08:12:31 +0530

    Time: 130 ms (27.78%) | Memory: 134.8 MB (11.11%) - LeetSync

--- 4000-count-bowl-subarrays/count-bowl-subarrays.java
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.java
@@ -1,45 +1,41 @@
 class Solution {
     public long bowlSubarrays(int[] nums) {
-        Stack <Integer> st = new Stack <> ();
-
-int [] nge =new int [nums.length];
-
-int [] pge = new int [nums.length]; 
-int counter=0;
-
-for (int i = 0 ; i < nums.length;i++){
+        Stack <Integer> st = new Stack <>();
+int nge []= new int [nums.length];
+int pge []= new int [nums.length]; 
+int count=0;
+for(int i = 0 ; i <nums.length;i++){
 nge[i]=-1;
 pge[i]=-1;
 }
 
+for(int i = 0 ; i < nums.length;i++ ){
+while (!st.isEmpty() && nums[st.peek()] < nums[i]){
+nge[st.pop()]=i;
+}
+st.push(i);
 
-
-
-for (int i = 0 ; i <nums.length; i++){
-while(!st.isEmpty() && nums[st.peek()] < nums[i] ){
-nge[st.peek()]=i;
-st.pop();
-
-} st.push(i);
 
 }
-st.clear() ; 
+st.clear();
 
-for (int i = nums.length-1 ; i >= 0; i--){
-while(!st.isEmpty() && nums[st.peek()] < nums[i] ){
-pge[st.peek()]=i;
-st.pop();
+for(int i = nums.length-1 ; i >=0; i--){
+while (!st.isEmpty() && nums[st.peek()] < nums[i]){
+pge[st.pop()]=i;
+}
+st.push(i);
 
-} st.push(i);
 
 }
 
+for(int i = 0 ; i <nums.length;i++){
 
-for(int i = 0 ; i < nums.length ; i++ ) {
-if(nge[i]!=-1 && pge[i] !=-1)  
- counter++;
-} 
+if(nge[i]+pge[i] >=2 && nge[i]!=-1 && pge[i]!=-1) {count++;}
 
-return counter;
+}
+
+return count ;
+
+        
     }
 }
